9ddcde70019edcf7876ccbc481bdf6ab15f134db
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2023-02-28 15:18:27 +0100

    getting ready to deploy

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const cors = require("cors");
+
+
+app.use(express.json());
+app.use(cors());
+
+
+const db = require("./models");
+
+// Routers
+const postRouter = require("./routes/posts");
+app.use("/posts", postRouter);
+const commentsRouter = require("./routes/comments");
+app.use("/comments", commentsRouter);
+const usersRouter = require("./routes/users");
+app.use("/auth", usersRouter);
+const likesRouter = require("./routes/likes");
+app.use("/likes", likesRouter);
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      `Server listening on ${PORT}`
+    );
+  });
+});
